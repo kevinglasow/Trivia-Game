b18f5cd8542962bfe80471ea9d74b5f093eca6b2
Kevin J Glasow <kevin.glasow@gmail.com>
2018-03-01 16:11:27 -0500

    update to css and psudocode

---
+++ assets/js/javascript.js
@@ -0,0 +1,13 @@
+// Create a function that we can call upon the page load
+
+alert(test)
+
+// Set a variable which will be the total time for the game in seconds
+
+// Create a timer that subtracts one from that variable every second
+
+// Push the variable to the timer div on game.html
+
+// Stop the countdown once it equals 0
+
+// Once the countdown timer hits zero replace the questions/answers with results
